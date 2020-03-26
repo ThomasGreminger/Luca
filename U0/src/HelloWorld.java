1ed64f1665c2a0fbebf8dad4413c1ed19f8fa785
thoma <thoma@192.168.0.227>
2020-03-26 18:45:50 +0100

    Everything

--- U0/src/HelloWorld.java
+++ U0/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     
     public static void main(String[] args) {
-        System.out.println("Hello World!  Geht ein:" + 1.);
+        System.out.println("Hello World!  Geht ein:" + 1.99);
     }
 }
