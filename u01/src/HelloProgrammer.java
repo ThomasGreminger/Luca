4a94087fac68cfce4363106ad40222ff32666a99
manna <manna@192.168.0.220>
2020-03-28 16:11:18 +0100

    ich habe meinen namen eingefügt.

--- u01/src/HelloProgrammer.java
+++ u01/src/HelloProgrammer.java
@@ -1,6 +1,6 @@
 
 public class HelloProgrammer {
-    
+    //ich habe meinen namen eingefügt.
     public static void main(String[] args) {
         System.out.println("Hello my name is Luca Manna!");
     }
