befe69fc1811595d5ab5fb73fef42335e15843f4
manna <manna@192.168.0.220>
2020-03-30 19:30:18 +0200

    Ich habe ein Teil der Aufgabe 5 gelöst. Das andere kahm noch nicht vor.

---
+++ u02/src/Extra.java
@@ -0,0 +1,22 @@
+
+public class Extra {
+
+	public static void main(String[] args) {
+		printLinie1();	// TODO Auto-generated method stub
+		printLinie2();
+		printLinie1();
+		printLinie2();
+		printLinie1();
+		printLinie2();
+		printLinie1();
+		printLinie2();
+		
+	}	//main
+	public static void printLinie1()	{
+		System.out.println("**..**..**..");
+	}	//Linie eins
+	
+	public static void printLinie2()	{
+		System.out.println("..**..**..**");
+	}
+}
