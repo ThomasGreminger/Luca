454c6941246d740c2e1644f37ca7d72c0dc36310
manna <manna@192.168.0.220>
2020-03-30 18:41:10 +0200

    Ich habe die Aufgabe 1 gemacht.

--- u02/src/ForwardAndBackward.java
+++ u02/src/ForwardAndBackward.java
@@ -1,6 +1,11 @@
 public class ForwardAndBackward {
     
     public static void main(String[] args) {
-        // TODO
-    }
+        System.out.println("    \\/");
+        System.out.println("   \\\\//");
+        System.out.println("  \\\\\\///");
+        System.out.println("  ///\\\\\\");
+        System.out.println("   //\\\\");
+        System.out.println("    /\\");
+    }	
 }
